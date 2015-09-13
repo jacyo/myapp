67f5d92e98ade59818de491a84bff70b49c2f5ab
zhangjian781215 <zhangjian781215@126.com>
2015-09-13 21:58:26 +0800

    test9

---
+++ file_exists.go
@@ -0,0 +1,15 @@
+package main
+
+import "os"
+
+// 判断文件是否存在
+func FileExists(file string) bool {
+	if _, err := os.Stat(file); err != nil {
+		return false
+	}
+	return true
+}
+
+func main() {
+    println(FileExists("file_exists.go"))
+}
